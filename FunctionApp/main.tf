8d9f194f3eca7d96af6810497666c69971e8e722
Arkadiusz Slawek <o_arkadiusz.s@kbtg.tech>
2023-11-16 01:38:57 +0100

    Added remote backend for statefile

--- FunctionApp/main.tf
+++ FunctionApp/main.tf
@@ -14,7 +14,17 @@ resource "azurerm_linux_function_app" "nn_fn_app" {
   storage_account_access_key = var.fnapp_strgacc_key
   service_plan_id = var.fnapp_svcplan_id
 
-  site_config {} 
+  site_config {
+    application_stack {
+      docker {
+        registry_url = "cgnn23registry.azurecr.io"
+        image_name = "tuzzoopythonsamplevscodeflasktutorial"
+        image_tag =  "latest"
+      }
+    }
+    container_registry_use_managed_identity = true
+    
+  } 
 
   tags = local.tags
 }
--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.78.0"
-    }
-  }  
-}
-
-provider "azurerm" {
-    features {}
-  
-}
 
 locals {
   name = "Ciraltos"
